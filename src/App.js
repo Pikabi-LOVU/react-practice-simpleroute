9b25ee84a3ec2f9ba1d7fe272274a89460569def
Pikabi-LOVU <pikabi.lovu@gmail.com>
2022-08-01 14:05:02 +0900

    Add App sample code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            MyPage
+          </li>
+          <li>
+            Dashboard
+          </li>
+        </ul>
+      </nav>
     </div>
-  );
+   </BrowserRouter>
+)
+}
+
+// Home 컴포넌트
+function Home() {
+  return <h1>Home</h1>;
+}
+
+// MyPage 컴포넌트
+function MyPage() {
+  return <h1>MyPage</h1>;
+}
+
+// Dashboard 컴포넌트
+function Dashboard() {
+  return <h1>Dashboard</h1>;
 }
 
 export default App;
